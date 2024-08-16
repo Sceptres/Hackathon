e6dc5a9a471dfa5c4bbdb9b4580f47621739402c
Beshoy Hezky <beshoyhezky@gmail.com>
2024-08-16 19:04:35 -0400

    Add conditional change

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useAuth } from '../../contexts/authContext'
+import { Navigate, Link } from 'react-router-dom'
+
 
 const Home = () => {
-    const { currentUser } = useAuth()
+    const { currentUser,userLoggedIn } = useAuth()
     return (
-        <div className='text-2xl font-bold pt-14'>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</div>
+        <div className='text-2xl font-bold pt-14'>
+            {!userLoggedIn ? (<Navigate to={'/login'} replace={true} />) : <div> Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</div>}
+
+           
+            </div>
     )
 }
 
