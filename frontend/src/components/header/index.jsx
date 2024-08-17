1183e74acdb7ace14435ffc8e30e959729c50319
Beshoy Hezky <beshoyhezky@gmail.com>
2024-08-16 20:56:21 -0400

    Add tailwind stuff

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -6,23 +6,25 @@ import { doSignOut } from '../../firebase/auth'
 const Header = () => {
     const navigate = useNavigate()
     const { userLoggedIn } = useAuth()
-    return (
-        <nav className='flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200'>
-            {
-                userLoggedIn
-                    ?
-                    <>
-                        <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className='text-sm text-blue-600 underline'>Logout</button>
-                    </>
-                    :
-                    <>
-                        <Link className='text-sm text-blue-600 underline' to={'/login'}>Login</Link>
-                        <Link className='text-sm text-blue-600 underline' to={'/register'}>Register New Account</Link>
-                    </>
-            }
 
-        </nav>
-    )
+    if (userLoggedIn) {
+        return (
+            <div className=" inline-block" style={{ width: '100%' }}>
+                <button 
+                    onClick={() => { 
+                        doSignOut().then(() => { 
+                            navigate('/login') 
+                        }) 
+                    }} 
+                    className="absolute top-1 right-1 text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block"
+                >
+                    Logout
+                </button>
+            </div>
+        )
+    }
+
+    return null
 }
 
 export default Header
--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { useAuth } from '../../contexts/authContext'
-import { Navigate, Link } from 'react-router-dom'
+import { doSignOut } from '../../firebase/auth'
+import { Link, useNavigate, Navigate } from 'react-router-dom'
 
 
 const Home = () => {
+    const navigate = useNavigate()
     const { currentUser,userLoggedIn } = useAuth()
     return (
         <div className='text-2xl font-bold pt-14'>
+                            <button 
+                    onClick={() => { 
+                        doSignOut().then(() => { 
+                            navigate('/login') 
+                        }) 
+                    }} 
+                    className="absolute top-1 right-1 text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block"
+                >
+                    Logout
+                </button>
             {!userLoggedIn ? (<Navigate to={'/login'} replace={true} />) : <div> Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</div>}
 
            
