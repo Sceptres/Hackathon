e874855ecb0c5538cdbfd2ba092419a1150ba269
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-19 17:14:58 -0400

    Add authentication checks on all routes

--- frontend/src/components/game/endGame.jsx
+++ frontend/src/components/game/endGame.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { useAuth } from '../../contexts/authContext'
 import { useNavigate, Navigate } from 'react-router-dom'
+import { auth } from '../../firebase/firebase'
 
 
 const EndGame = () => {
     const navigate = useNavigate()
 
-    return (
-        <div>Game Over!
+    const currentUser = auth.currentUser;
 
-            Stats
-
-            <button onClick={() => {navigate('/home')}}
-                className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
-                Home
-            </button>
-        </div>
-    );   
+    if(!currentUser) {
+        return <Navigate to={'/login'} replace={true} />;
+    } else {
+        return (
+            <div>Game Over!
+    
+                Stats
+    
+                <button onClick={() => {navigate('/home')}}
+                    className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
+                    Home
+                </button>
+            </div>
+        );
+    }   
 }
 
 export default EndGame
--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { useAuth } from '../../contexts/authContext'
 import { useNavigate, Navigate } from 'react-router-dom'
 import { getDate, dateToStringFormat, formatNumberToUSD, getGamePortfilio } from '../../help/help'
 import StockUI from './stockUI'
+import { auth } from '../../firebase/firebase'
 
 /**
  * 
@@ -106,7 +106,7 @@ async function sellStock(ticker, quantity, date, gameId) {
 }
 
 const Game = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const gameId = 'yRHqAIZsbiHvqu9ONC1t';
 
     const [searchTerm, setSearchTerm] = useState();
@@ -220,87 +220,93 @@ const Game = () => {
         toggleSellPopup();
     };
 
-    return (
-        <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
-            <div className="text-2xl font-bold pt-14">
-                <button onClick={() => { navigate('/endgame') }}
-                    className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
-                    End Game
-                </button>
-                <button onClick={() => { navigate('/guide') }}
-                    className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
-                    Read the Investment Guide!
-                </button>
-            </div>
+    const currentUser = auth.currentUser;
 
-            <div className="text-xl font-semibold mt-4">
-                Balance: {portfolio && portfolio.balance && formatNumberToUSD(portfolio.balance)}
-            </div>
-
-            <div className="bg-white w-full flex-grow flex flex-col items-center">
-                <form className="flex items-center max-w-sm mx-auto mt-8">
-                    <input type="date" id="stock" name="stock" value={seletedDate} onChange={handleDateChange} className="mr-5"></input>
-
-                    <label htmlFor="simple-search" className="sr-only">Search</label>
-                    <div className="relative w-full">
-                        <input
-                            type="text"
-                            id="simple-search"
-                            value={searchTerm}
-                            onChange={handleChange}
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search Stock name..."
-                            required
-                        />
-                        {showDropdown && filteredOptions.length > 0 && (
-                            <ul className="absolute z-10 w-full mt-2 bg-white border border-gray-300 rounded-lg shadow-lg dark:bg-gray-700 dark:border-gray-600">
-                                {filteredOptions.map((option, index) => (
-                                    <li
-                                        key={index}
-                                        onClick={() => handleOptionClick(option['symbol'])}
-                                        className="px-4 py-2 cursor-pointer hover:bg-blue-500 hover:text-white dark:hover:bg-blue-600 dark:text-white"
-                                    >
-                                        {option['symbol']}:{option['name']}
-                                    </li>
-                                ))}
-                            </ul>
-                        )}
-                    </div>
-                    <button onClick={onSearchApi} className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                        <span className="sr-only">Search</span>
+    if(!currentUser) {
+        return <Navigate to={'/login'} replace={true} />;
+    } else {
+        return (
+            <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
+                <div className="text-2xl font-bold pt-14">
+                    <button onClick={() => { navigate('/endgame') }}
+                        className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
+                        End Game
                     </button>
-                </form>
-
-                <div className="flex-grow w-full max-w-6xl mt-4 flex items-center justify-center" style={{ maxHeight: 'calc(100vh - 20rem)' }}>
-                    <div className="w-full">
-                        <StockUI ticker={selectedOption} currentDate={seletedDate} stockPriceUpdater={setCurrentStockPrice} />
+                    <button onClick={() => { navigate('/guide') }}
+                        className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
+                        Read the Investment Guide!
+                    </button>
+                </div>
+    
+                <div className="text-xl font-semibold mt-4">
+                    Balance: {portfolio && portfolio.balance && formatNumberToUSD(portfolio.balance)}
+                </div>
+    
+                <div className="bg-white w-full flex-grow flex flex-col items-center">
+                    <form className="flex items-center max-w-sm mx-auto mt-8">
+                        <input type="date" id="stock" name="stock" value={seletedDate} onChange={handleDateChange} className="mr-5"></input>
+    
+                        <label htmlFor="simple-search" className="sr-only">Search</label>
+                        <div className="relative w-full">
+                            <input
+                                type="text"
+                                id="simple-search"
+                                value={searchTerm}
+                                onChange={handleChange}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Search Stock name..."
+                                required
+                            />
+                            {showDropdown && filteredOptions.length > 0 && (
+                                <ul className="absolute z-10 w-full mt-2 bg-white border border-gray-300 rounded-lg shadow-lg dark:bg-gray-700 dark:border-gray-600">
+                                    {filteredOptions.map((option, index) => (
+                                        <li
+                                            key={index}
+                                            onClick={() => handleOptionClick(option['symbol'])}
+                                            className="px-4 py-2 cursor-pointer hover:bg-blue-500 hover:text-white dark:hover:bg-blue-600 dark:text-white"
+                                        >
+                                            {option['symbol']}:{option['name']}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                        <button onClick={onSearchApi} className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                            <span className="sr-only">Search</span>
+                        </button>
+                    </form>
+    
+                    <div className="flex-grow w-full max-w-6xl mt-4 flex items-center justify-center" style={{ maxHeight: 'calc(100vh - 20rem)' }}>
+                        <div className="w-full">
+                            <StockUI ticker={selectedOption} currentDate={seletedDate} stockPriceUpdater={setCurrentStockPrice} />
+                        </div>
+                    </div>
+    
+                    <div className="flex justify-center space-x-4 mt-4 w-full max-w-4xl">
+                        <button
+                            onClick={onSellClick}
+                            className="w-1/3 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+                            style={{ maxWidth: '150px' }} // Adjust this value as needed
+                        >
+                            Sell
+                        </button>
+                        {isSellPopupOpen && <Popup title={selectedOption} stockPrice={currentStockPrice} maxValue={portfolio.stocks[selectedOption]} startValue={0} buttonName={'Sell'} buttonClass={'bg-red-500 text-white rounded-lg hover:bg-red-600'} onClose={onSellPopupClosed} toggleMethod={toggleSellPopup} />}
+                        <button
+                            onClick={onBuyClick}
+                            className="w-1/3 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
+                            style={{ maxWidth: '150px' }} // Adjust this value as needed
+                        >
+                            Buy
+                        </button>
+                        {isBuyPopupOpen && <Popup title={selectedOption} stockPrice={currentStockPrice} maxValue={Math.floor(portfolio && currentStockPrice && portfolio.balance/currentStockPrice)} startValue={0} buttonName={'Buy'} buttonClass={'bg-green-500 text-white rounded-lg hover:bg-green-600'} onClose={onBuyPopupClosed} toggleMethod={toggleBuyPopup} />}
                     </div>
                 </div>
-
-                <div className="flex justify-center space-x-4 mt-4 w-full max-w-4xl">
-                    <button
-                        onClick={onSellClick}
-                        className="w-1/3 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
-                        style={{ maxWidth: '150px' }} // Adjust this value as needed
-                    >
-                        Sell
-                    </button>
-                    {isSellPopupOpen && <Popup title={selectedOption} stockPrice={currentStockPrice} maxValue={portfolio.stocks[selectedOption]} startValue={0} buttonName={'Sell'} buttonClass={'bg-red-500 text-white rounded-lg hover:bg-red-600'} onClose={onSellPopupClosed} toggleMethod={toggleSellPopup} />}
-                    <button
-                        onClick={onBuyClick}
-                        className="w-1/3 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
-                        style={{ maxWidth: '150px' }} // Adjust this value as needed
-                    >
-                        Buy
-                    </button>
-                    {isBuyPopupOpen && <Popup title={selectedOption} stockPrice={currentStockPrice} maxValue={Math.floor(portfolio && currentStockPrice && portfolio.balance/currentStockPrice)} startValue={0} buttonName={'Buy'} buttonClass={'bg-green-500 text-white rounded-lg hover:bg-green-600'} onClose={onBuyPopupClosed} toggleMethod={toggleBuyPopup} />}
-                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Game
