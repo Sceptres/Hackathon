e165d0b5e2e6f77e4e010b023548cd339c235944
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-19 01:57:50 -0400

    Remove auth check in game and endgame to temp avoid errors

--- frontend/src/components/game/endGame.jsx
+++ frontend/src/components/game/endGame.jsx
@@ -5,25 +5,18 @@ import { useNavigate, Navigate } from 'react-router-dom'
 
 const EndGame = () => {
     const navigate = useNavigate()
-    const {isUserLoggedIn} = useAuth()
 
-    if(!isUserLoggedIn) {
-        return (
-            <Navigate to={'/login'} replace={true} />
-        );
-    } else {
-        return (
-            <div>Game Over!
-    
-                Stats
-    
-                <button onClick={() => {navigate('/home')}}
-                    className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
-                    Home
-                </button>
-            </div>
-        );
-    }    
+    return (
+        <div>Game Over!
+
+            Stats
+
+            <button onClick={() => {navigate('/home')}}
+                className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
+                Home
+            </button>
+        </div>
+    );   
 }
 
 export default EndGame
