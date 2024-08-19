7d0aa53d233f603e921fb6ef59936b48d9b0bf37
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-19 19:23:56 -0400

    Add nav button in game back to leaderboard

--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -246,6 +246,10 @@ const Game = () => {
         return (
             <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
                 <div className="text-2xl font-bold pt-14">
+                    <button onClick={() => { navigate('/home') }}
+                        className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
+                        Leaderboard
+                    </button>
                     <button onClick={() => { navigate('/endgame') }}
                         className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
                         End Game
