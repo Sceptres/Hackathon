3b27009782729bf2c6d5023c2a373b3a216d1009
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-19 01:20:14 -0400

    Add auth to game and endgame routes

--- frontend/src/components/game/endGame.jsx
+++ frontend/src/components/game/endGame.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { useAuth } from '../../contexts/authContext'
-import { doSignOut } from '../../firebase/auth'
-import { Link, useNavigate, Navigate } from 'react-router-dom'
+import { useNavigate, Navigate } from 'react-router-dom'
 
 
 const EndGame = () => {
     const navigate = useNavigate()
-    return (
-        <div>Game Over!
+    const {isUserLoggedIn} = useAuth()
 
-            Stats
-
-            <button onClick={() => {navigate('/home')}}
-                className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
-                Home
-            </button>
-        </div>
-    )
+    if(!isUserLoggedIn) {
+        return (
+            <Navigate to={'/login'} replace={true} />
+        );
+    } else {
+        return (
+            <div>Game Over!
+    
+                Stats
+    
+                <button onClick={() => {navigate('/home')}}
+                    className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900 inline-block">
+                    Home
+                </button>
+            </div>
+        );
+    }    
 }
 
 export default EndGame
