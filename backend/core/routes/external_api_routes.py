d44d0e94835b0141e25d2cd0078e4cdd56669277
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-20 00:14:37 -0400

    Remove news API endpoint

--- backend/core/routes/external_api_routes.py
+++ backend/core/routes/external_api_routes.py
@@ -5,27 +5,7 @@ import datetime as dt
 
 from ..external_api.factory.api_service_factory import APIServiceFactory
 
-from ..external_api.api_types import ApiTypes
-
-
-
-@external_api_blueprint.route('/get_news', methods=['POST'])
-async def get_news():
-    try:
-        request_data = request.get_json()
-    
-        news_api = APIServiceFactory.create_api_client(ApiTypes.News)
-        data = await news_api.get_stock_news(request_data['ticker'])
-        response = jsonify(data)
-        response.status_code = 200
-        return response
-    except Exception as e:
-        response = jsonify(str(e))
-        response.status_code = 500
-        return response
-
-    
-
+from ..external_api.api_types import ApiTypes 
 
 @external_api_blueprint.route('/get_stock_by_date', methods=['POST'])
 async def get_stock_by_date():
