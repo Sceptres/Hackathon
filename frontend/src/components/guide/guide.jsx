0ed8c3c3a045a55be7f90fe0f835f81833f41c82
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-19 22:54:47 -0400

    Add auth check to guide route

--- frontend/src/components/guide/guide.jsx
+++ frontend/src/components/guide/guide.jsx
@@ -1,17 +1,24 @@
 import { useNavigate, Navigate } from 'react-router-dom'
 import GuideMarkdown from './markdown'
+import { auth } from '../../firebase/firebase'
 
 const Guide = () => {
     const navigate = useNavigate()
-    return (
-        <div>
+    const currentUser = auth.currentUser;
+
+    if(!currentUser) {
+        return <Navigate to={'/login'} replace={true} />;
+    } else {
+        return (
             <div>
-                <button className="mt-2 w-full py-2 bg-gray-200 text-gray-700 rounded" onClick={() => {navigate('/home')}}>Home</button>
+                <div>
+                    <button className="mt-2 w-full py-2 bg-gray-200 text-gray-700 rounded" onClick={() => {navigate('/home')}}>Home</button>
+                </div>
+                <GuideMarkdown></GuideMarkdown>
+                
             </div>
-            <GuideMarkdown></GuideMarkdown>
-            
-        </div>
-    )
+        );
+    }
 }
 
 export default Guide
