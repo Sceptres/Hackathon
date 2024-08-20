df3738f06e1bbeadb10e162f56e966d7ebdd1817
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-20 00:12:55 -0400

    Remove new API

--- backend/core/external_api/api_types.py
+++ backend/core/external_api/api_types.py
@@ -2,4 +2,3 @@ from enum import Enum
 
 class ApiTypes(Enum):
     Stock = 'Stock'
-    News = 'News'
--- backend/core/external_api/factory/api_service_factory.py
+++ backend/core/external_api/factory/api_service_factory.py
@@ -1,13 +1,10 @@
 from ..api_types import ApiTypes
 from ..services.stock_service import StockApiService
-from ..services.news_service import NewsApiService
 
 class APIServiceFactory:
     @staticmethod
     def create_api_client(api_type:ApiTypes):
         if api_type == ApiTypes.Stock:
             return StockApiService()
-        elif api_type == ApiTypes.News:
-            return NewsApiService()
         else:
             raise Exception("Wrong Api Type ")
