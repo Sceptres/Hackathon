2265fbf13c31af194010262765f2ad867d7ff740
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-19 22:50:31 -0400

    Remove header import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
-import Header from "./components/header";
 import Home from "./components/home";
 import Game from "./components/game/game";
 import EndGame from "./components/game/endGame";
